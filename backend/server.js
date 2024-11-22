fe07689eef7a3eef8821dbc42f6830f37b05057a
Ezgi Deren <ezgi_deren@yahoo.com>
2024-11-22 20:29:47 +0100

    api endpoints updated

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import authRoutes from './src/routes/authRoutes.js';
 import menuRoutes from './src/routes/menuRoutes.js';
 import orderRoutes from './src/routes/orderRoutes.js';
 import tableRoutes from './src/routes/tableRoutes.js';
+import dotenv from 'dotenv';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './src/config/database.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const rootDir = join(__dirname, '..');
 
+dotenv.config({ path: join(rootDir, '.env') });
+
+//Initialize Express
 const app = express();
 
-// CORS and middleware setup
 const corsOptions = {
-    origin: ['https://order-management-system-turkishtale.onrender.com', 'http://localhost:5173'],
+    origin: 'https://order-management-system-turkishtale-uudf.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
